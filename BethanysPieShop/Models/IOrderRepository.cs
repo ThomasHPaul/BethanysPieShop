363b723612d0691afc487217ed0a1be2b31c4fc3
ThomasHPaul <tpaul19@wgu.edu>
2020-03-02 15:10:43 -0600

    Added Order Form.

---
+++ BethanysPieShop/Models/IOrderRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class IOrderRepository
+    {
+        public void CreateOrder(Order order)
+        {
+
+        }
+    }
+}
